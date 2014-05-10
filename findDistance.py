441b3bb75e785541d8d2009e3b0f69a3185bedd2
Sean Teeling <teeling.sean@gmail.com>
2014-05-10 16:20:29 -0700

    working

--- findDistance.py
+++ findDistance.py
@@ -3,35 +3,36 @@
 from bs4 import BeautifulSoup
 import re
 
-primary_words = ["Dorthey"]
-secondary_words = ["1904"]
+primary_words = ["Birth"]
 def parse_doc(doc):
 	distances = []			#[(primary_word, secondary, distance), ]
 
-	soup = BeautifulSoup(doc)
+	soup = BeautifulSoup(doc.lower())
 	
-	for p_word in primary_words:								#grab each hotword
+	for p_word in primary_words:	
+		p_word = p_word.lower()							#grab each hotword
 		for p_elem in soup(text=re.compile(p_word)): 			#grab each occurrence of the hot word
+			print p_elem
 			p_tag = p_elem.parent.name
 			min_distance = -1
-			for s_word in secondary_words:						#compare to each secondary word
-				for s_elem in soup(text=re.compile(s_word)):		#grab each occurrence of the secondary word
-					distance = -1
-					print "hello", s_elem, p_elem, s_elem.name, p_elem.name
-					if s_elem == p_elem:
-						distance = calc_same_tag_distance(p_elem,s_elem,p_word,s_word)
-					elif (s_elem.parent.name.lower() == 'td' or s_elem.parent.name.lower() == 'th') and (p_elem.parent.name.lower() == 'td' or p_elem.parent.name.lower() == 'th') and s_elem.parent.parent == p_elem.parent.parent: 
-						distance = 1
-					print distance
-					if distance == -1:
-						continue
-					#do some distance manipulation
-					if distance < min_distance or min_distance == -1:
-						print "in here"
-						min_distance = distance
-						min_s_word = s_word
-						s_tag = s_elem.parent.name
-			distances.append((p_word,min_s_word,min_distance,p_tag,s_tag))	
+			#for s_word in secondary_words:						#compare to each secondary word
+			for s_elem in soup(text=re.compile(r'\b((1[4-9]\d\d)|(20((1[0-4])|(0\d))))\b')):		#grab each occurrence of the secondary word
+				print s_elem
+				distance = -1
+				if s_elem == p_elem:
+					distance = calc_same_tag_distance(p_elem,s_elem,p_word,s_word)
+				elif (p_elem.parent.name.lower() == 'th' and s_elem.parent.name.lower() == 'td' and p_elem.parent.parent.parent == s_elem.parent.parent.parent) or ((s_elem.parent.name.lower() == 'td' or s_elem.parent.name.lower() == 'th') and (p_elem.parent.name.lower() == 'td' or p_elem.parent.name.lower() == 'th') and s_elem.parent.parent == p_elem.parent.parent): 
+					distance = 1
+					print "HELLO", p_elem, s_elem
+				if distance == -1:
+					continue
+				#do some distance manipulation
+				if distance < min_distance or min_distance == -1:
+					min_distance = distance
+					min_s_word = s_elem
+					s_tag = s_elem.parent.name
+			if min_distance != -1:
+				distances.append((p_word,min_s_word,min_distance,p_tag,s_tag))	
 	return distances		
 
 def calc_same_tag_distance(p_elem, s_elem, p_word, s_word):
--- testDistance.py
+++ testDistance.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 
 import findDistance
-
+from bs4 import BeautifulSoup
+import re
 f = open("temp.html", "r")
 doc = f.read()
 print findDistance.parse_doc(doc)
+
+
+soup = BeautifulSoup(doc.lower())
+
+temp = soup(text=re.compile(r'\b((1[4-9]\d\d)|(20((1[0-4])|(0\d))))\b'))
